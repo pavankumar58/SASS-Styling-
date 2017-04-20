7a930ce972c591b751184fd6ec3eddee518bceec
pavankumar58 <pavankumarkolluri21@gmail.com>
2017-04-20 19:53:49 -0400

    SASS tutorials

---
+++ config.rb
@@ -0,0 +1,13 @@
+require 'sass-globbing'
+require 'singularitygs'
+require 'breakpoint'
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = ""
+css_dir =""
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
+
